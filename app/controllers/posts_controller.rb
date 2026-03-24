d16a9f7d1d90afe42cdadc49be25358fef4582e8
blizzard115 <fubuki.sato@gmail.com>
2026-03-24 16:03:36 +0900

    コントローラー修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create] # ログインしているかどうかを判断
+  before_action :authenticate_user!, only: [:new, :create, :destroy] # ログインしているかどうかを判断
  
   def index
-    @posts = Post.limit(10).order(created_at: :desc) #作成日時が新しい順
+    @posts = Post.where(is_public: true).order(created_at: :desc).limit(10) #作成日時が新しい順
   end
  
   def new
@@ -11,34 +11,37 @@ class PostsController < ApplicationController
  
   def create
     @post = Post.new(post_params) # ストロングパラメータを使ってフォームから受け取ったパラメータを許可
-    @post.user_id = current_user.id # ログインユーザのIDを代入して関連付け
+    @post.user = current_user
  
     if @post.save
       flash[:notice] = '投稿しました' # 成功時のフラッシュメッセージ
       redirect_to posts_path # 一時的にトップページへリダイレクト(後に修正)
     else
       flash[:alert] = '投稿に失敗しました' # 失敗時のフラッシュメッセージ
-      render :new # 投稿画面を再表示
+      render :new, status: :unprocessable_entity # 投稿画面を再表示
     end
   end
  
   def show
-    @post = Post.find_by(id: params[:id])
+    @post = Post.find(id: params[:id])
   end
  
   def destroy
-    post = Post.find_by(id: params[:id])
+    post = Post.find(params[:id])
+
     if post.user == current_user
       post.destroy
       flash[:notice] = "投稿が削除されました"
+    else
+      flash[:alert] = "他のユーザーの投稿は削除できません"
     end
-     redirect_to posts_path
+
+    redirect_to posts_path
   end
- 
   private
  
   # ストロングパラメータで許可するカラムを指定
   def post_params
-    params.require(:post).permit(:title, :content) # title と content のみ許可
+    params.require(:post).permit(:title, :content, :post_type, :status, :is_public)
   end
 end
